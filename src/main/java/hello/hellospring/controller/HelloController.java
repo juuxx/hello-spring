4975e98fcadeadc8e685d217f6b4df6286f33e5e
myeongjooseo <juux_x@naver.com>
2021-05-17 00:39:18 +0900

    2강 - 스프링 웹개발 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +13,37 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","히히히히!!!!!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody//http에서 header / body 에서 body부에 이 내용을 직접 넣어주겠다.
+    public String helloString(@RequestParam("name")String name){
+        return "hello" + name; //"hello Spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public  Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); //cmd + shift + enter 치면 나머지 자동완성
+        hello.setName(name);
+        return hello; //문자가 아닌 객체 넘기기 -> json 방식으로 보여줌
+    }
+
+    static class Hello {
+        String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
