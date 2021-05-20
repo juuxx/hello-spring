58238a361c20b028e9bc3fe9d8d703cdb8d43e82
myeongjooseo <juux_x@naver.com>
2021-05-20 20:33:49 +0900

    spring data JPA

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -6,9 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
-    @Override
-    Member save(Member member);
-
+    //JPQL select m from Member m where m.name = ? <- JPQL을 자동으로 생성해줌
     @Override
     Optional<Member> findByName(String name);
 }
